018943cab0d0f390c01a8ead96b75a27ea5b65d0
LiliSLG <silvialilianagarcia@hotmail.com>
2023-05-10 19:17:27 -0300

    Ejercicio Corregido

--- 05.js
+++ 05.js
@@ -14,8 +14,15 @@ function pluck(array, propiedad) {
   // Pista: es una buena oportunidad para usar map.
 
   // Tu código acá:
-
+  let soloPropideadDeseada = array.map(function(element) {
+    return element[propiedad]
+  })
+  return soloPropideadDeseada
 }
+
+//var productos = [{ name: 'TV LCD', price: 100}, { name: 'Computadora', price: 500 }]
+//console.log(pluck(productos, 'name')) //debería devolver ['TV LCD', 'Computadora']
+  
 
 // No modifiques nada debajo de esta linea //
 
